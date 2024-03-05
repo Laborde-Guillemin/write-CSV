d24aa59795cd3450f2aacfb192ee9b0c98d77877
Laborde <labordeguillem1@gmail.com>
2024-03-05 20:25:13 +0100

    maj

---
+++ Write/src/main.cpp
@@ -0,0 +1,90 @@
+/*
+   ______               _                  _///_ _           _                   _
+  /   _  \             (_)                |  ___| |         | |                 (_)
+  |  [_|  |__  ___  ___ _  ___  _ __      | |__ | | ___  ___| |_ _ __ ___  _ __  _  ___  _   _  ___
+  |   ___/ _ \| __|| __| |/ _ \| '_ \_____|  __|| |/ _ \/  _|  _| '__/   \| '_ \| |/   \| | | |/ _ \
+  |  |  | ( ) |__ ||__ | | ( ) | | | |____| |__ | |  __/| (_| |_| | | (_) | | | | | (_) | |_| |  __/
+  \__|   \__,_|___||___|_|\___/|_| [_|    \____/|_|\___|\____\__\_|  \___/|_| |_|_|\__  |\__,_|\___|
+                                                                                      | |
+                                                                                      \_|
+  Fichier :       programme-test-bme280.ino
+  
+  Description :   Programme permettant de tester un BME280 branché sur les pins SDA et SCL d'un Arduino (bus I2C)
+
+  Auteur :        Jérôme TOMSKI (https://passionelectronique.fr/)
+  Créé le :       30.09.2021
+
+  Librairie Adafruit BME280 utilisée : https://github.com/adafruit/Adafruit_BME280_Library (importée depuis le Gestionnaire de bibliothèques Arduino)
+
+*/
+
+#include <Adafruit_BME280.h>                            // Inclusion de la librairie BME280 d'Adafruit
+
+// Constantes du programme
+#define adresseI2CduBME280                0x76            // Adresse I2C du BME280 (0x76, dans mon cas, ce qui est souvent la valeur par défaut)
+#define pressionAuNiveauDeLaMerEnHpa      1024.90         // https://fr.wikipedia.org/wiki/Pression_atmospherique (1013.25 hPa en moyenne, valeur "par défaut")
+#define delaiRafraichissementAffichage    1500            // Délai de rafraîchissement de l'affichage (en millisecondes)
+
+// Instanciation de la librairie BME280
+Adafruit_BME280 bme;
+
+
+// ========================
+// Initialisation programme
+// ========================
+void setup() {
+  
+  // Initialisation du port série (pour l'envoi d'infos via le moniteur série de l'IDE Arduino)
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("Programme de test du BME280");
+  Serial.println("===========================");
+  Serial.println();
+
+  // Initialisation du BME280
+  Serial.print(F("Initialisation du BME280, à l'adresse [0x"));
+  Serial.print(adresseI2CduBME280, HEX);
+  Serial.println(F("]"));
+  
+  if(!bme.begin(adresseI2CduBME280)) {
+    Serial.println(F("--> ÉCHEC…"));
+    while(1);                              // Arrêt du programme, en cas d'échec de l'initialisation
+  } else {
+    Serial.println(F("--> RÉUSSIE !"));
+  }
+  Serial.println();
+  
+}
+
+
+// ======================================
+// Boucle principale (boucle perpétuelle)
+// ======================================
+void loop() {
+
+  // Affichage de la TEMPÉRATURE
+  Serial.print(F("Température = "));
+  Serial.print(bme.readTemperature());
+  Serial.println(F(" °C"));
+
+  // Affichage du TAUX D'HUMIDITÉ
+  Serial.print(F("Humidité = "));
+  Serial.print(bme.readHumidity());
+  Serial.println(F(" %"));
+  
+  // Affichage de la PRESSION ATMOSPHÉRIQUE
+  Serial.print(F("Pression atmosphérique = "));
+  Serial.print(bme.readPressure() / 100.0F);
+  Serial.println(F(" hPa"));
+
+  // Affichage de l'estimation d'ALTITUDE
+  Serial.print(F("Altitude estimée = "));
+  Serial.print(bme.readAltitude(pressionAuNiveauDeLaMerEnHpa));
+  Serial.println(F(" m"));
+
+
+  // ... et on répète ce cycle à l'infini !
+  delay(delaiRafraichissementAffichage);                // Avec x secondes d'attente, avant chaque rebouclage
+  Serial.println();                                     // … et un saut de ligne pour faire joli ;)
+
+}
